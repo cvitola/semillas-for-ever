33ebedf546557a5f0c326a0f144b80bd48aa491c
Ezequiel <85136487+Mondone@users.noreply.github.com>
2022-04-10 21:07:51 -0300

    Primer punto avances

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -27,6 +27,25 @@ class Soja(var altura: Double, val anioSemilla: Int) {
     fun esFuerte(): Boolean {
         TODO("Así me implementas mejor")
     }
+
+    fun toleranciaSol(): Double {
+        if (altura<0.5){ return 6.0 }
+        else if ((altura>=0.5) && (altura<=1.0)) { return 8.0 }
+        else { return 12.0 }
+    }
+}
+class Quinoa(var altura: Double, val anioSemilla: Int, val espacio: Double) {
+    fun espacio(): Double {
+        return espacio
+    }
+
+    fun daSemillas(): Boolean {
+        TODO("Dalee")
+    }
+
+    fun esFuerte(): Boolean {
+        TODO("Así me implementas mejor")
+    }
 }
 
 class Planta(var altura: Double, val anioSemilla: Int) {
--- src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
@@ -10,8 +10,10 @@ class SemillasTest : DescribeSpec ({
     // anioSemilla, altura
     describe("Creación de las plantas") {
         val menta = Menta(1.0, 2021)
-        val mentita = Menta(0.3, 2021)
+        val menta2 = Menta(0.3, 2021)
         val soja = Soja(0.6, 2009)
+        val quinoa = Quinoa(1.0,2010,0.2)
+        val quinoa2 = Quinoa(1.0,2006, 0.9)
 
         it("probamos los atributos altura  y anioSemilla") {
             menta.altura.shouldBe(1.0)
@@ -20,8 +22,10 @@ class SemillasTest : DescribeSpec ({
 
         it("verificar si da semillas") {
             menta.daSemillas().shouldBeTrue()
-            mentita.daSemillas().shouldBeFalse()
+            menta2.daSemillas().shouldBeFalse()
             soja.daSemillas().shouldBeFalse()
+            quinoa.daSemillas().shouldBeTrue()
+            quinoa2.daSemillas().shouldBeTrue()
         }
 
         it("es fuerte") {
@@ -31,17 +35,24 @@ class SemillasTest : DescribeSpec ({
 
         it("espacio") {
             menta.espacio().shouldBe(2.0)
-            mentita.espacio().shouldBe(1.3)
+            menta2.espacio().shouldBe(1.3)
             soja.espacio().shouldBe(0.3)
+            quinoa.espacio().shouldBe(0.2)
+            quinoa2.espacio().shouldBe(0.9)
+        }
+
+        it("horas de sol toleradas"){
+            soja.toleranciaSol().shouldBe(8.0)
         }
 
         it("verifico la suma de varias") {
             val superficie = mutableListOf(
                 soja.espacio(),
                 menta.espacio(),
-                mentita.espacio()
+                menta2.espacio()
             ).sum()
             Math.ceil(superficie).shouldBe(4)
         }
+
     }
 })
